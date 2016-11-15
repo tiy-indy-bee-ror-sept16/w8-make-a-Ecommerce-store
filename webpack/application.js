99f1e00acf5ce246496bae33ef90cc56b69ff0ea
Manpreet Singh <mannyy91s@gmail.com>
2016-11-15 14:04:04 -0500

    setup react boiler plate stuff in app.js, added index and thanks.html pages

--- webpack/application.js
+++ webpack/application.js
@@ -1,2 +1,13 @@
-console.log("Hey");
 import React from "react"
+import ReactDOM from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router'
+import Home from '/components/Home'
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    ReactDOM.render(
+        <Router history={browserHistory}>
+            <Route path="/" component={Home} />
+        </Router>
+        ,document.getElementById('app')
+    )
+})
