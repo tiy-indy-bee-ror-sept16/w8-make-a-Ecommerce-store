23056b2e01044a4a7ebdaf6a82aa42efd0ff32cb
keith brooks <kbrook10@gmail.com>
2016-11-15 17:51:34 -0500

    Update migration due to error

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,21 @@ ActiveRecord::Schema.define(version: 20161115213109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "adonis_schema", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.integer  "batch"
+    t.datetime "migration_time"
+  end
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.string   "message",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "line_items", force: :cascade do |t|
