a7a038706f5e0e978e3bc3684b371af96a33f461
Manpreet Singh <mannyy91s@gmail.com>
2016-11-15 14:42:35 -0500

    added boiler plater for admin, checkout, payment, and singleitem components.

---
+++ webpack/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Checkout extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default Checkout
---
+++ webpack/components/SingleItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class SingleItem extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default SingleItem
