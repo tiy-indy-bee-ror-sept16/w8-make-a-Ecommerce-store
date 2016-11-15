c7b180b756d6e8590a4454306d816543b615c0b2
llancon <rdzalb@gmail.com>
2016-11-15 15:27:49 -0500

    Made changesto patches controller

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20161115190611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "line_items", force: :cascade do |t|
     t.integer  "quantity"
     t.string   "size"
@@ -23,8 +22,6 @@ ActiveRecord::Schema.define(version: 20161115190611) do
     t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> ba1700073011a892a5fd09b00d4805ebbd26097d
   create_table "patches", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
