52e233cf820035eb4306a6c387d6bad6b61b08fe
Young Mi Kim <ymkim23@gmail.com>
2016-11-15 16:17:41 -0500

    did all html for admin

--- webpack/components/Admin.js
+++ webpack/components/Admin.js
@@ -5,7 +5,30 @@ class Admin extends Component {
         super(props)
     }
     render(){
-        return (
+        return (<nav className="navbar  navbar-fixed-top navbar-default">
+        <div className="container">
+          <div className="col-sm-6">
+            <h4>Iron Glory</h4>
+          </div>
+          <div className="col-sm-6">
+            <button id="logout" type="submit" className="btn btn-default text-right">Log Out</button>
+          </div>
+        </div>
+      </nav>
+
+
+      <div className="container panelBody">
+        <div className="row">
+          <div className="col-sm-3">
+            <h3>Items</h3>
+            <h3>Customer</h3>
+            <h3>Edit Item</h3>
+          </div>
+          <div className="col-sm-9">
+          </div>
+        </div>
+
+      </div>
 
         )
     }
