36d118402b0ab461e3f2ec48516404f7a2c694a2
TIYZAP <ironyardzap@gmail.com>
2016-11-15 22:35:03 -0500

    backend debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
 class UsersController < ApplicationController
 
-  before_action :require_user
-
-  def index
-    @users = User.all
-    render json: @users
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.cart = Cart.new
-    if @user.save
-      render json: @user
-    else
-      render json: @user.errors.full_messages, status: :unprocessable_entity
-    end
-  end
-
-  def user_params
-    params.permit(:email, :password)
-  end
+  # before_action :require_user
+  #
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
+  #
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.cart = Cart.new
+  #   if @user.save
+  #     render json: @user
+  #   else
+  #     render json: @user.errors.full_messages, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # def user_params
+  #   params.permit(:email, :password)
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :password
-  has_many :carts
+  # validates_presence_of :email, :password
+  # has_many :carts
 
 end
