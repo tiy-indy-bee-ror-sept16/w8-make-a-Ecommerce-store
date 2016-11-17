8eda65c9a6ec7d8b997a63f7b0ddcb911c1f98e6
Manpreet Singh <mannyy91s@gmail.com>
2016-11-17 13:59:13 -0500

    fixed patch.rb issue

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -4,17 +4,9 @@ class Patch < ApplicationRecord
   has_many :line_items
   belongs_to :category
   attachment :image
-<<<<<<< HEAD
-  belongs_to :category
-  monetize :price_cents
-||||||| merged common ancestors
-  monetize :price_cents
-=======
 
 
   # scope :name, -> { where(:name true) }
   # pg_search_scope :search_by_name, {:associated_against => against: :name}
-
->>>>>>> f739aa095a9b66bc27e8d8f23d2f742aaab722fd
 
 end
