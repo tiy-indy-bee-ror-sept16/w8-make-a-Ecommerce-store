85aa57b8ffaece50bb8ac89c13cf85267f46e609
keith brooks <kbrook10@gmail.com>
2016-11-17 12:59:56 -0500

    Update due to merge issues with db/schema.rb and app/models/patch.rb for singleItem branch

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -3,14 +3,5 @@ class Patch < ApplicationRecord
   has_many :line_items
   belongs_to :category
   attachment :image
-<<<<<<< HEAD
-||||||| merged common ancestors
-<<<<<<< HEAD
-  belongs_to :category
-=======
 
->>>>>>> b0a1fd866e435df366c887c252b8b9e8414b1cd8
-=======
-
->>>>>>> 76b026f469ade8c950080154e0af8ec8e6cc3758
 end
