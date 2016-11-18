5ca2515020c90bd1a722bbe0ec238162e293d7cf
llancon <rdzalb@gmail.com>
2016-11-18 13:50:50 -0500

    fixed syntax error in thankyou mailer

--- app/mailers/thankyou_mailer.rb
+++ app/mailers/thankyou_mailer.rb
@@ -2,6 +2,7 @@ class ThankyouMailer < ApplicationMailer
 
   def thank_you(cart)
 	@cart = cart
-	mail(to: @cart.email, subject: “Welcome to whatever”)
+	mail(:to => @cart.email,
+  :subject => 'Thank you for shopping with us!')
   end
 end
