47c01f3294026851c7c0a9dd6b087dcfa4b330ee
TIYZAP <ironyardzap@gmail.com>
2016-11-18 13:40:00 -0500

    debug routes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,19 +1,8 @@
 Rails.configuration.stripe = {
 
   :publishable_key => ENV["stripe_publishable_key"],
-<<<<<<< HEAD
+
   :secret_key      => ENV["stripe_secret_key"],
-
-||||||| merged common ancestors
-  :secret_key      => ENV["stripe_secret_key"]
-
-=======
-  :publishable_key => ENV["stripe_publishable_key"],
-  :secret_key      => ENV["stripe_secret_key"]
->>>>>>> c758cef05caf7503a432ac951ac94294f9cd6568
-=======
-  :secret_key      => ENV["stripe_secret_key"]
->>>>>>> cca927c9077ce73e556c98e5ac013c71ffe5f7d7
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
