2e7658453bd2c10127d644f99a837bcb9ccef85e
llancon <rdzalb@gmail.com>
2016-11-18 11:00:50 -0500

    Added name column to cart so customer can add name on order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118023613) do
+ActiveRecord::Schema.define(version: 20161118155339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161118023613) do
     t.string   "ship_to_address"
     t.string   "email"
     t.boolean  "complete",        default: false, null: false
+    t.string   "name"
   end
 
   create_table "categories", force: :cascade do |t|
